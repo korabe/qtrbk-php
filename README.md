a39a09f9ce1f0f8a9156e123db64593b4a15dfc2
Bryon Bean <bryonbean@gmail.com>
2020-07-19 14:31:16 -0400

    doc: updates README.md

--- README.md
+++ README.md
@@ -1,2 +1,32 @@
 # qtrbk-php
 PHP Bindings for Qtrbk
+
+## Install
+
+```php
+composer install korabe/qtrbk-php
+```
+
+## Example
+
+```php
+<?php
+
+require "vendor/autoload.php";
+
+use QtrBk\QtrBk;
+
+$qtrbk = new QtrBk();
+
+// XML file installed with the QuickBooks SDK
+$host_query_xml = 'C:\Program Files (x86)\Intuit\IDN\QBSDK13.0\samples\xmlfiles\HostQueryRq.xml';
+$qbxml_rq = file_get_contents($host_query_xml);
+$qtrbk->process($qbxml_rq);
+
+if ($qtrbk->hasErrors()) {
+    echo "{$qtrbk->getErrors()}\n";
+    exit($qtrbk->getExitCode());
+}
+
+echo "{$qtrbk->getQbXmlResponse()}\n";
+```
--- test.php
+++ test.php
@@ -1,43 +1,20 @@
 <?php
+
 require "vendor/autoload.php";
 
-if (!extension_loaded("ffi")) {
-    throw new Exception("Could not load FFI: extension not found.");
-}
-
-use QtrBk\QBXML\Document;
-use QtrBk\QBXML\HostQuery;
 use QtrBk\QtrBk;
 
 $qtrbk = new QtrBk();
-$doc = new Document();
-$msg = new HostQuery();
 
-$xml = $doc->addElement($msg)
-    ->render();
+// XML file installed with the QuickBooks SDK
+$host_query_xml = 'C:\Program Files (x86)\Intuit\IDN\QBSDK13.0\samples\xmlfiles\HostQueryRq.xml';
+$qbxml_rq = file_get_contents($host_query_xml);
+$qtrbk->process($qbxml_rq);
 
-$response = $qtrbk->process($xml);
-
-if ($response->isOk()) {
-    echo $response->json();
-} else {
-    echo $response->getError();
+if ($qtrbk->hasErrors()) {
+    echo "{$qtrbk->getQbXmlResponse()}\n";
+    echo "{$qtrbk->getErrors()}\n";
+    exit($qtrbk->getExitCode());
 }
 
-//$ffi = FFI::cdef(<<<EOH
-//struct qtrbk {
-//    char * request;
-//	const char * response;
-//	const char * error;
-//	int err;
-//};
-//char * process_request(struct qtrbk * qb);
-//const struct qtrbk* create_qtrbk(const char * xml);
-//
-//EOH, "QtrBkLib.dll");
-//
-//$host_query_path = 'C:\Program Files (x86)\Intuit\IDN\QBSDK13.0\samples\xmlfiles\HostQueryRq.xml';
-//$xml  = file_get_contents($host_query_path);
-//$qb = $ffi->create_qtrbk($xml);
-//$ffi->process_request($qb);
-//echo FFI::string($qb->response);
+echo "{$qtrbk->getQbXmlResponse()}\n";
