7d6626150cbf2211ee5f64897fab8db10267fd6d
Bryon Bean <bryonbean@gmail.com>
2020-09-06 21:09:29 -0400

    feat: adds DateMacro property

---
+++ src/QBXML/Queries/Properties/DateMacro.php
@@ -0,0 +1,50 @@
+<?php declare(strict_types=1);
+
+namespace QtrBk\QBXML\Queries\Properties;
+
+use Spatie\Enum\Enum;
+
+/**
+ * Class DateMacro
+ * @package QtrBk\QBXML\Queries\Properties
+ *
+ * @method static self All
+ * @method static self Today
+ * @method static self ThisWeek
+ * @method static self ThisWeekToDate
+ * @method static self ThisMonth
+ * @method static self ThisMonthToDate
+ * @method static self ThisCalendarQuarter
+ * @method static self ThisCalendarQuarterToDate
+ * @method static self ThisFiscalQuarter
+ * @method static self ThisFiscalQuarterToDate
+ * @method static self ThisCalendarYear
+ * @method static self ThisCalendarYearToDate
+ * @method static self ThisFiscalYear
+ * @method static self ThisFiscalYearToDate
+ * @method static self Yesterday
+ * @method static self LastWeek
+ * @method static self LastWeekToDate
+ * @method static self LastMonth
+ * @method static self LastMonthToDate
+ * @method static self LastCalendarQuarter
+ * @method static self LastCalendarQuarterToDate
+ * @method static self LastFiscalQuarter
+ * @method static self LastFiscalQuarterToDate
+ * @method static self LastCalendarYear
+ * @method static self LastCalendarYearToDate
+ * @method static self LastFiscalYear
+ * @method static self LastFiscalYearToDate
+ * @method static self NextWeek
+ * @method static self NextFourWeeks
+ * @method static self NextMonth
+ * @method static self NextCalendarQuarter
+ * @method static self NextCalendarYear
+ * @method static self NextFiscalQuarter
+ * @method static self NextFiscalYear
+ */
+class DateMacro extends Enum
+{
+}
+
+//
